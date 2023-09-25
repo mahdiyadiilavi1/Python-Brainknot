c08800696d8c54b455d41c53780f7e15c4224c02
mahdoosh1 <83725163+mahdiyadiilavi1@users.noreply.github.com>
2023-09-25 17:33:32 +0330

    Update and rename brainfuck.py to brainknot.py

---
+++ brainknot.py
@@ -0,0 +1,90 @@
+def execute(filename):
+  f = open(filename, "r")
+  evaluate(f.read())
+  f.close()
+
+
+def evaluate(code,inputs,stack0=[0],cellptr=0,crbit=0):
+  code     = cleanup(list(code))
+  bracemap = buildbracemap(code)
+  inptr = 0
+  outputs = []
+  codeptr = 0
+
+  while codeptr < len(code):
+    command = code[codeptr]
+    tmp = 0
+    if command == "+":
+      stack0[cellptr] = crbit
+      cellptr += 1
+      if cellptr == len(stack0): stack0.append(0)
+
+    if command == "-":
+      crbit = stack0[cellptr]
+      cellptr -= 1
+    if command == "*":
+      crbit = 1 - crbit
+    if command == "(" and crbit == 0: codeptr = bracemap[codeptr]
+    if command == ")" and crbit != 0: codeptr = bracemap[codeptr]
+    if command == "<":
+      outputs.append(str(crbit))
+    if command == ">":
+      if len(inputs)==inptr:
+        return [stack0,cellptr,crbit,''.join(outputs)]
+      crbit = int(inputs[inptr])
+      inptr += 1
+    if command == "[":
+      if crbit == 1:
+        for p,i in enumerate(code[codeptr+1:]):
+          if i == "[":
+            tmp += 1
+          if i == "]":
+            tmp -= 1
+          if i == "," and tmp == 0:
+            comma = p+codeptr+1
+          if i == "]" and tmp == -1:
+            finish = p+codeptr+1
+        stack0,codeptr,crbit = evaluate(code[codeptr+1:comma],inputs,stack0,cellptr,crbit)
+      else:
+        for p,i in enumerate(code[codeptr+1:]):
+          if i == "[":
+            tmp += 1
+          if i == "]":
+            tmp -= 1
+          if i == "," and tmp == 0:
+            comma = p+codeptr+1
+          if i == "]" and tmp == -1:
+            finish = p+codeptr+1
+        stack0,codeptr,crbit = evaluate(code[comma:finish],inputs,stack0,cellptr,crbit)
+      codeptr = finish-1
+    codeptr += 1
+  return [stack0,cellptr,crbit,''.join(outputs)]
+
+
+def cleanup(code):
+  return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '*', '(', ')', '+', '-'], code))
+
+
+def buildbracemap(code):
+  temp_bracestack, bracemap = [], {}
+
+  for position, command in enumerate(code):
+    if command == "(": temp_bracestack.append(position)
+    if command == ")":
+      start = temp_bracestack.pop()
+      bracemap[start] = position
+      bracemap[position] = start
+  return bracemap
+
+
+def main():
+  while 1:
+    code=input("Code:")
+    if code != '':
+      prevcode=code
+    else:
+      code=prevcode
+    print("Output:",evaluate(code=code,inputs=input("Inputs:"))[3])
+
+if __name__ == "__main__": main()
+
